f7964c74b785f96b943de515260618212073dc3a
Christofer Karankevits <chris.karankevits@gmail.com>
2020-09-18 11:22:14 +0300

    String

---
+++ src/trenering/App.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trenering;
+
+/**
+ *
+ * @author user
+ */
+class App {
+    public void run() {
+        System.out.println("---String---");
+        String text1 = "Съешь ещё этих французских булок и галет, да выпей чаю с молоком, как и я.";
+        char[] text1Array = text1.toCharArray();
+        String text2 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя;
+        char[] text1Array = text2.toCharArray();
+        boolean a = true;
+        System.out.println(text1Array);
+        System.out.println(text2Array);
+        
+        
+    }
+}
